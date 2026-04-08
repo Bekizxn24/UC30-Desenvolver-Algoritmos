6cd2381655fcced8a43f59116ca6e85d6c050957
Bekizxn24 <Lorenrebecca7@gmail.com>
2026-04-08 08:31:54 -0300

    Refactor variable names for consistency

--- UC30-08.04/ativ.py
+++ UC30-08.04/ativ.py
@@ -1,23 +1,23 @@
 print("Vamos calcular quantos pontos a equipe conseguiu!")
 
 
-P = int(input("Digite o número de pães vendidos: "))
-D = int(input("Digite o número de doces vendidos: "))
-B = int(input("Digite o número de bolos vendidos: "))
+p = int(input("Digite o número de pães vendidos: "))
+d = int(input("Digite o número de doces vendidos: "))
+b = int(input("Digite o número de bolos vendidos: "))
 
-pontos = P * 1 + D * 2 + B * 3
+pontos = p * 1 + d * 2 + b * 3
 print(f"\nA pontuação total da semana foi: {pontos} pontos")
 
 
 if pontos >= 150:
     print(" A equipe ganhou um bolo como prêmio! 🎂")
-    print("Resultado final: B")
+    print("Resultado final: b")
 elif pontos >= 120:
     print(" A equipe ganhou um doce como prêmio! 🍬")
-    print("Resultado final: D")
+    print("Resultado final: d")
 elif pontos >= 100:
     print(" A equipe ganhou um pão como prêmio! 🍞")
-    print("Resultado final: P")
+    print("Resultado final: p")
 else:
     print("Infelizmente não foi dessa vez, não há prêmio dessa vez. 😕")
-    print("Resultado final: N")
+    print("Resultado final: n")
